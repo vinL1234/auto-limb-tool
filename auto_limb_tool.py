fd5e93f07415f7825d75b30d05bce9f65f7dc934
vinL1234 <158228863+vinL1234@users.noreply.github.com>
2026-07-19 18:28:49 -0600

    Update auto_limb_tool.py

--- auto_limb_tool.py
+++ auto_limb_tool.py
@@ -2,4 +2,10 @@
 
 from auto_limb_tool import show
 
+# Launch the tool when this file runs in Maya's Script Editor.
+show()
+"""Convenience launcher for running from the Maya Script Editor."""
+
+from auto_limb_tool import show
+
 show()
